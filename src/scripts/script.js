e90e5c9de4941d1247b21d071cdb17e4b617ee9d
Marcos Santana <mg7163551@gmail.com>
2025-10-24 22:02:29 -0300

    finalizando

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,6 +3,8 @@ function menu(){
     let menu = document.getElementById('menu')
     if(n == 0){
       menu.style.display = 'flex'
+      menu.style.flexDirection = 'column'
+      menu.style.justifyContent = 'space-between'
       n = 1
     }else{
       menu.style.display = 'none'
